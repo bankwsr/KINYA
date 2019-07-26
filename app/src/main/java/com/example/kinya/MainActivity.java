e2ebbfba4fe35d6c256aad93e1177c358500f1f3
bankwsr <WIsarut25388@gmail.com>
2019-07-26 12:50:02 +0700

    **อกแบบหน้า Login แบบใหม่** 1. Status Bar แบบใส 2. พื้นหลังแบบไล่สี 3. แก้ไข AppTheme บน manifests

--- app/src/main/java/com/example/kinya/MainActivity.java
+++ app/src/main/java/com/example/kinya/MainActivity.java
@@ -3,78 +3,42 @@ package com.example.kinya;
 import android.content.Intent;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.drawable.AnimationDrawable;
+import android.os.Build;
 import android.os.Bundle;
 import androidx.appcompat.widget.Toolbar;
+
+import android.util.Log;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.ProgressBar;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.GoogleAuthProvider;
 
 public class MainActivity extends AppCompatActivity {
-
-    /*Toolbar toolbar;
-    ProgressBar progressBar;
-    EditText email, password;
-    Button signup, signin;
-
-    FirebaseAuth firebaseAuth;
-
-    @Override
-    protected void onCreate(Bundle saveInstanceState) {
-        super.onCreate(saveInstanceState);
-        setContentView(R.layout.activity_main);
-
-        toolbar = findViewById(R.id.toolbar);
-        progressBar = findViewById(R.id.progressBar);
-        email = findViewById(R.id.etEmail);
-        password = findViewById(R.id.etPassword);
-        signup = findViewById(R.id.btnSignup);
-        signin = findViewById(R.id.btnSignin);
-
-        toolbar.setTitle(R.string.app_name);
-
-        firebaseAuth = firebaseAuth.getInstance();
-
-        signup.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                progressBar.setVisibility(View.VISIBLE);
-                firebaseAuth.createUserWithEmailAndPassword(email.getText().toString(), password.getText().toString())
-                        .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
-                            @Override
-                            public void onComplete(@NonNull Task<AuthResult> task) {
-                                progressBar.setVisibility(View.GONE);
-                                if (task.isSuccessful()) {
-                                    Toast.makeText(MainActivity.this, "Registered!!", Toast.LENGTH_LONG).show();
-                                    email.setText("");
-                                    password.setText("");
-                                } else {
-                                    Toast.makeText(MainActivity.this, task.getException().getMessage(), Toast.LENGTH_LONG).show();
-                                }
-                            }
-                        });
-            }
-        });
-
-        signin.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View view) {
-                startActivity(new Intent(MainActivity.this, RegisActivity.class));
-            }
-        });
-    }
-}*/
 
     Toolbar toolbar;
     ProgressBar progressBar;
     EditText userEmail, userPassword;
     Button userSignin, userSignup;
+    GoogleSignInClient mGoogleSignInClient;
 
     FirebaseAuth firebaseAuth;
 
@@ -83,13 +47,32 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        toolbar = findViewById(R.id.toolbar2);
+        //toolbar = findViewById(R.id.toolbar2);
         //toolbar.setTitle("Login");
         progressBar = findViewById(R.id.progressBar);
         userEmail = findViewById(R.id.etUserEmail);
         userPassword = findViewById(R.id.etUserPassword);
         userSignin = findViewById(R.id.btnUserSignin);
         userSignup = findViewById(R.id.btnSignup);
+
+        LinearLayout linearLayout = findViewById(R.id.mainLayout);
+        AnimationDrawable animationDrawable = (AnimationDrawable) linearLayout.getBackground();
+        animationDrawable.setEnterFadeDuration(2000);
+        animationDrawable.setExitFadeDuration(4000);
+        animationDrawable.start();
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            Window w = getWindow();
+            w.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
+        }
+
+        GoogleSignInOptions googleSignInOptions = new GoogleSignInOptions
+                .Builder()
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
+
+        mGoogleSignInClient = GoogleSignIn.getClient(this,googleSignInOptions);
 
         firebaseAuth = FirebaseAuth.getInstance();
 
@@ -119,4 +102,35 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+
+   /* void SignInGoogle(){
+        progressBar.setVisibility(View.VISIBLE);
+        Intent signIntent = mGoogleSignInClient.getSignInIntent();
+        startActivityForResult(signIntent, GOOGLE_SIGN);
+    }
+
+    @Override
+    public void onActivityReenter(int resultCode, Intent data) {
+        super.onActivityReenter(resultCode, data);
+        if (requestCode == GOOGLE_SIGN){
+            Task<GoogleSignInAccount> task = GoogleSignIn
+                    .getSignedInAccountFromIntent(data);
+            try{
+                GoogleSignInAccount account = task.getResult(ApiException.class);
+                if(account != null) firebaseAuthWithGoogle(account);
+            }catch(ApiException e){
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private void firebaseAuthWithGoogle(GoogleSignInAccount account) {
+        Log.d("TAG","firebaseAuthWithGoogle: " + account.getId());
+        AuthCredential credential = GoogleAuthProvider.
+                getCredential(account.getIdToken(),null);
+        mAuth.signInWithCredential.(credential)
+            .addOnCompleteListener(this,task -> {
+                if (task.isSuccessfull);
+            })
+    }*/
 }
--- app/src/main/java/com/example/kinya/RegisActivity.java
+++ app/src/main/java/com/example/kinya/RegisActivity.java
@@ -4,10 +4,12 @@ import android.content.Intent;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ProgressBar;
+import android.widget.TextView;
 import android.widget.Toast;
 import android.widget.Toolbar;
 
@@ -18,54 +20,14 @@ import com.google.firebase.auth.FirebaseAuth;
 
 public class RegisActivity extends AppCompatActivity {
 
-    /*Toolbar toolbar;
-    ProgressBar progressBar;
-    EditText userEmail, userPassword;
-    Button userSignin;
-
-    FirebaseAuth firebaseAuth;
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_regis);
-
-        //toolbar = findViewById(R.id.toolbar2);
-        //toolbar.setTitle("Login");
-        progressBar = findViewById(R.id.progressBar);
-        userEmail = findViewById(R.id.etUserEmail);
-        userPassword = findViewById(R.id.etUserPassword);
-        userSignin = findViewById(R.id.btnUserSignin);
-
-        firebaseAuth = FirebaseAuth.getInstance();
-
-        userSignin.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                progressBar.setVisibility(View.VISIBLE);
-                firebaseAuth.signInWithEmailAndPassword(userEmail.getText().toString(), userPassword.getText().toString())
-                        .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
-                            @Override
-                            public void onComplete(@NonNull Task<AuthResult> task) {
-                                if (task.isSuccessful()) {
-                                    Toast.makeText(RegisActivity.this, "ยินดีต้อนรับ", Toast.LENGTH_LONG).show();
-                                    startActivity(new Intent(RegisActivity.this, ProfileActivity.class));
-                                } else {
-                                    Toast.makeText(RegisActivity.this, task.getException().getMessage(),
-                                            Toast.LENGTH_LONG).show();
-                                }
-                            }
-                        });
-            }
-        });
-    }
-}
-*/
-
     Toolbar toolbar;
     ProgressBar progressBar;
     EditText email, password;
     Button signup, signin;
+
+    TextView textView;
+    Handler handler = new Handler();
+    private int progressStatus = 0;
 
     FirebaseAuth firebaseAuth;
 
@@ -76,6 +38,8 @@ public class RegisActivity extends AppCompatActivity {
 
         //toolbar = findViewById(R.id.toolbar);
         progressBar = findViewById(R.id.progressBar);
+        textView = findViewById(R.id.textView);
+
         email = findViewById(R.id.etEmail);
         password = findViewById(R.id.etPassword);
         signup = findViewById(R.id.btnSignup);
@@ -94,6 +58,29 @@ public class RegisActivity extends AppCompatActivity {
                             @Override
                             public void onComplete(@NonNull Task<AuthResult> task) {
                                 progressBar.setVisibility(View.GONE);
+                                new Thread(new Runnable() {
+                                    public void run() {
+                                        while (progressStatus < 100) {
+                                            progressStatus += 1;
+                                            // Update the progress bar and display the
+                                            //current value in the text view
+                                            handler.post(new Runnable() {
+                                                public void run() {
+                                                    progressBar.setProgress(progressStatus);
+                                                    textView.setText(progressStatus+"/"+progressBar.getMax());
+                                                }
+                                            });
+                                            try {
+                                                // Sleep for 200 milliseconds.
+                                                Thread.sleep(200);
+                                            } catch (InterruptedException e) {
+                                                e.printStackTrace();
+                                            }
+                                        }
+                                    }
+                                }).start();
+
+
                                 if (task.isSuccessful()) {
                                     Toast.makeText(RegisActivity.this, "Registered!!", Toast.LENGTH_LONG).show();
                                     email.setText("");
